fdc866f35f349ccc088ace7b9ec5ded0f9c6c7fc
Abh1P <purohit.abhi36@gmail.com>
2026-04-08 23:53:00 +0100

    initial project setup with JAX-RS and structure

---
+++ src/main/java/w2072553/abhipurohit/csa/api/cw/CSAAPICW.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package w2072553.abhipurohit.csa.api.cw;
+
+/**
+ *
+ * @author abhipurohit
+ */
+public class CSAAPICW {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
